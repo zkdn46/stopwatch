06b491d04039dd43c367d98dac5fd13957fa107e
user <user@DESKTOP-980EADG>
2024-11-13 21:52:37 +0900

    Correct Time

--- src/stopwatch/Time.java
+++ src/stopwatch/Time.java
@@ -12,10 +12,7 @@ public class Time extends Thread {
 	protected static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	protected static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 	protected static SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
-	
-	
 
-	
 	protected int sec = 1;
 	protected boolean isRun = true;
 
@@ -24,22 +21,20 @@ public class Time extends Thread {
 			try {
 				if (isRun) {
 					Calendar cal = Calendar.getInstance();
-					buffer.append(timeFormat.format(cal.getTime()));
 
+					buffer.append(timeFormat.format(cal.getTime()));
 					buffer.append(String.format(" [%d sec]\n", sec++));
 					writer.append(buffer);
 					buffer.setLength(0);
-
 					writer.flush();
-				} 
+				}
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
-	            break;
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-			
-			
+				break;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
 		}
 
 	}
