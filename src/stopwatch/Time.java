4e16032ac1870c3076659398fd131f6698fd61bf
zkdn46 <123gusckd123@naver.com>
2024-11-13 16:52:45 +0900

    Add Time run()

--- src/stopwatch/Time.java
+++ src/stopwatch/Time.java
@@ -5,34 +5,37 @@ import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class Time implements Runnable {
-
+public class Time extends Thread {
 	private StringBuffer buffer = new StringBuffer();
 	private BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
 	private SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
 
+	protected int sec = 1;
+	protected boolean isRun = true;
+
 	public void run() {
-		
+
 		while (true) {
-
 			try {
-				Thread.sleep(1000);
-				
-				Calendar cal = Calendar.getInstance();
-				buffer.append(timeFormat.format(cal.getTime()));
-				writer.append(buffer);
-				
-				buffer.setLength(0);
-				
-				writer.flush();
-				System.out.println();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
-		}
-		
-	}
+				if (isRun) {
+					Calendar cal = Calendar.getInstance();
+					buffer.append(timeFormat.format(cal.getTime()));
 
+					buffer.append(String.format(" [%d sec]\n", sec++));
+					writer.append(buffer);
+					buffer.setLength(0);
+
+					writer.flush();
+				} else {
+					Calendar cal = Calendar.getInstance();
+					buffer.append(timeFormat.format(cal.getTime()));
+					buffer.setLength(0);
+				}
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				break;
+			}
+		}
+
+	}
 }
